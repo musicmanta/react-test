a163cf1bc39527f00dd349c349805bcfd6be91a7
musicmanta <96392825+musicmanta@users.noreply.github.com>
2022-12-25 17:41:33 +0000

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Random Visitor!</h1>
+      <p>You found this secret URL. Congratulations!</p>
+      <h2>How to start learning React.js</h2>
+      <p>Here is a story of how I learnt React.js. My prior knowledge of javascript was rather <strong>limited.</strong></p>
+      <button>Click</button>
+    </div>
+  );
+}
