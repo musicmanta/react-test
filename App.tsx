7fa0b412dce921cb52b1b6a080e561348d030c7e
musicmanta <96392825+musicmanta@users.noreply.github.com>
2022-12-25 17:43:01 +0000

    1 file modified

--- App.tsx
+++ App.tsx
@@ -7,7 +7,10 @@ export default function App() {
       <h1>Hello Random Visitor!</h1>
       <p>You found this secret URL. Congratulations!</p>
       <h2>How to start learning React.js</h2>
-      <p>Here is a story of how I learnt React.js. My prior knowledge of javascript was rather <strong>limited.</strong></p>
+      <p>
+        Here is the story of how I learnt React.js. My prior knowledge of
+        javascript was rather <strong>limited.</strong>
+      </p>
       <button>Click</button>
     </div>
   );
